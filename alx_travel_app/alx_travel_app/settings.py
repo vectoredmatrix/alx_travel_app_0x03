d4f1a1dd9c5054fea8fa4e68847c5ced03c640ff
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-11-06 03:47:06 +0100

    modified gitignore

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8fop+t7sb^jbt*fybwo=+q5x9tj!1)woe1hy(!0s(oz5$ye#3'
+SECRET_KEY = os.getenv("DJANGOSECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
